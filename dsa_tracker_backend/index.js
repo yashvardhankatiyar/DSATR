d701193a3f6d96e93c1f6e361d604e08ad37329a
yashvardhankatiyar <ykatiyar0902@gmail.com>
2024-07-08 11:46:55 +0530

    backend ready

--- dsa_tracker_backend/index.js
+++ dsa_tracker_backend/index.js
@@ -6,11 +6,9 @@ const authMiddleware = require('./middleware/authMiddleware'); // Import the mid
 const path = require('path');
 
 const server = express();
-server.use(express.static(path.join(__dirname, 'public')));
 
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+
+
 
 const questionRouter = require('./Routes/question');
 const topicRouter = require('./Routes/topic');
